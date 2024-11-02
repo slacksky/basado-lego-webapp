3e291f6e9ce9b124c3928f904c1f2d8012294d8e
Jorge Vivas <slacksky@gmail.com>
2024-11-01 23:36:42 -0400

    t3 fix

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ const legoData = require("./modules/legoSets");
 const app = express();
 const PORT = process.env.PORT || 3000; // port by env var?
 //const PORT = 3000;
-//app.set("view engine", "ejs");// ejs as the viewing engine
+app.set("view engine", "ejs");// ejs as the viewing engine
 //app.set("views", path.join(__dirname, "views"));//adapting to vercel?
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + 'views');
 
 
 // Middleware to serve static files from the "public" folder
